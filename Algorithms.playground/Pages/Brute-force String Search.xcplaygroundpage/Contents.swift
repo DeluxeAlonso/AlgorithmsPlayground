5227f3c363f66ddd1637dc6a50c734e1c7d50ff5
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-07-08 01:29:56 -0500

    Adds brute-force search string code

--- Algorithms.playground/Pages/Brute-force String Search.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Brute-force String Search.xcplaygroundpage/Contents.swift
@@ -2,6 +2,52 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+extension String {
+
+    func indexOf(_ pattern: String) -> String.Index? {
+        for i in self.indices {
+            var j = i
+            var found = true
+            for p in pattern.indices{
+                if j == self.endIndex || self[j] != pattern[p] {
+                    found = false
+                    break
+                } else {
+                    j = self.index(after: j)
+                }
+            }
+            if found {
+                return i
+            }
+        }
+        return nil
+    }
+
+    func contains2(_ pattern: String) -> Bool {
+        for i in self.indices {
+            var j = i
+            var found = true
+            for p in pattern.indices {
+                if self[j] == pattern[p] {
+                    j = self.index(after: j)
+                } else {
+                    found = false
+                    break
+                }
+            }
+            if found {
+                return true
+            }
+        }
+        return false
+    }
+
+}
+
+let indices = "AAAA".indices
+
+"hola".indexOf("ola")
+"hola".contains2("ola")
+"hola".contains2("no")
 
 //: [Next](@next)
