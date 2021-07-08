836b0231e6eba63e2e29d80678610cebd490c882
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-07-08 01:20:26 -0500

    Updates Tree page

--- Algorithms.playground/Pages/Tree.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Tree.xcplaygroundpage/Contents.swift
@@ -22,4 +22,59 @@ public class Node<T> {
 
 }
 
+public class BinaryNode<T> {
+
+    public var value: T
+    public var left: BinaryNode?
+    public var right: BinaryNode?
+
+    public init(_ value: T) {
+        self.value = value
+    }
+
+    public func traverseInOrder(visit: (T) -> Void) {
+        left?.traverseInOrder(visit: visit)
+        visit(value)
+        right?.traverseInOrder(visit: visit)
+    }
+
+}
+
+public struct BinarySearchTree<T: Comparable> {
+
+    public private(set) var root: BinaryNode<T>?
+
+    public init() {}
+
+    public func containtsInOrder(_ value: T) -> Bool {
+        guard let root = root else {
+            return false
+        }
+
+        var found = false
+        root.traverseInOrder {
+            if $0 == value {
+                found = true
+            }
+        }
+
+        return found
+    }
+
+    public func contains(_ value: T) -> Bool {
+        var current = root
+        while let node = current {
+            if node.value == value {
+                return true
+            }
+            if value < node.value {
+                current = node.left
+            } else {
+                current = node.right
+            }
+        }
+        return false
+    }
+}
+
 //: [Next](@next)
