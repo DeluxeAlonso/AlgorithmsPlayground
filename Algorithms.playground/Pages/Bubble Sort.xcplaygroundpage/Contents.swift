8a5b94693b0a6cb47deebff5950cc53560501846
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-02 00:24:12 -0500

    Updates loop in Bubble Sort page

--- Algorithms.playground/Pages/Bubble Sort.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Bubble Sort.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 
 func bubbleSort<T: Comparable>(_ elements: [T]) -> [T] {
     var elements = elements
-    for i in 0..<elements.count {
+    elements.forEach { _ in
         for j in 1..<elements.count {
             if elements[j] < elements[j-1] {
                 elements.swapAt(j, j-1)
