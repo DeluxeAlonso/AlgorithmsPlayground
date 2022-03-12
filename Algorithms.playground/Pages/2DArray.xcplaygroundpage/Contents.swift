1ee77aa2d440a0fb7c7ea8eac769798691845fb1
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-03-12 01:40:52 -0500

    Creates page for 2 dim array in Swift

---
+++ Algorithms.playground/Pages/2DArray.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Create a 2 dimensional array 9 x 7
+
+var twoDimArray = [[Int]]()
+
+for _ in 1...9 {
+    var row = [Int]()
+    for _ in 1...7 {
+        row.append(0)
+    }
+    twoDimArray.append(row)
+}
+
+//: [Next](@next)
