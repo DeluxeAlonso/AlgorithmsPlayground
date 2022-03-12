647aa2321acfa36cbdf87d76e390d7e34f75d71d
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-03-12 01:48:05 -0500

    Creates page for diagonal difference exercise

---
+++ Algorithms.playground/Pages/Diagonal Difference.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+func diagonalDifference(arr: [[Int]]) -> Int {
+    let count = arr.count
+    var firstDiagonalSum = 0
+    var secondDiagonalSum = 0
+    for index in 0..<count {
+        firstDiagonalSum += arr[index][index]
+        secondDiagonalSum += arr[index][count-index-1]
+    }
+    return abs(firstDiagonalSum - secondDiagonalSum)
+}
+
+//: [Next](@next)
