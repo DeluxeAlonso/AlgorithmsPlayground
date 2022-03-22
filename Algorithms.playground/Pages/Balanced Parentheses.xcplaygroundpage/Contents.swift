6c6451edcf8edc93dfeeaae9296be6530c48a1d5
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-03-22 01:37:18 -0500

    Adds Balanced parentheses challenge page

---
+++ Algorithms.playground/Pages/Balanced Parentheses.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+//: [Previous](@previous)
+
+import Foundation
+
+struct Stack<T> {
+
+    private var elements: [T]
+
+    init(elements: [T]) {
+        self.elements = elements
+    }
+
+    mutating func push(_ element: T) {
+        elements.append(element)
+    }
+
+    mutating func pop() -> T? {
+        elements.removeLast()
+    }
+
+    var count: Int { return elements.count }
+
+}
+
+func isBalanced(_ text: String) -> Bool {
+    var stack = Stack<Character>(elements: [])
+    for character in text {
+        if character == "(" {
+            stack.push(character)
+        } else if character == ")" {
+            if stack.count == 0 {
+                return false
+            } else {
+                stack.pop()
+            }
+        }
+    }
+    return stack.count == 0
+}
+
+print(isBalanced("((()))"))
+//: [Next](@next)
