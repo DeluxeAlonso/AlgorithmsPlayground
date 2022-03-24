6fa078eeae663763e2b4d1d92b6917790d9d0231
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-03-23 23:08:11 -0500

    Implements maximum function in MinimumMaximum page

--- Algorithms.playground/Pages/MinimumMaximum.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/MinimumMaximum.xcplaygroundpage/Contents.swift
@@ -10,3 +10,19 @@ func minimum<T: Comparable>(_ array: [T]) -> T? {
     }
     return minimum
 }
+
+func maximum<T: Comparable>(_ array: [T]) -> T? {
+    guard var maximum = array.first else {
+        return nil
+    }
+    for element in array.dropFirst() {
+        if element > maximum {
+            maximum = element
+        }
+    }
+    return maximum
+}
+
+let array = [ 8, 3, 9, 4, 6 ]
+minimum(array)   // This will return 3
+maximum(array)   // This will return 9
