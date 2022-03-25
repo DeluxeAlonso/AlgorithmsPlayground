071b8f40ddbafe8dd3ef3a29b8d4f6bfbaa3af2f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-03-25 00:58:30 -0500

    Removes unnecesary comments in Diagonal Difference page

--- Algorithms.playground/Pages/Diagonal Difference.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Diagonal Difference.xcplaygroundpage/Contents.swift
@@ -20,7 +20,7 @@ var input = [
 
 diagonalDifference(arr: input)
 
-let years = [15,2,67, 4,5]
+let years = [15, 2, 67, 4, 5]
 let sum = years.filter { $0 < 10 }.map { $0 * 2 }.reduce(5, +)
 
 print(sum)
