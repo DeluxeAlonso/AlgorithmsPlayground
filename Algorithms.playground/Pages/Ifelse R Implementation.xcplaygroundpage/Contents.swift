4dff4713a52dc25a62e1c73027a38e47911e4e34
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-01-08 00:25:35 -0500

    Implements ifelse method using type-erased any type

--- Algorithms.playground/Pages/Ifelse R Implementation.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Ifelse R Implementation.xcplaygroundpage/Contents.swift
@@ -1,6 +1,16 @@
 // We ant to implement an ifelse() statement like the statistical programming language R uses
-// Exmaple: ifelse(condition, valueTrue, valueFalse)
+// Example: ifelse(condition, valueTrue, valueFalse)
 
-func iselse(_ condition: Bool, _ valueTrue: Int, _ valueFalse: Int) -> Int {
+func ifelse(_ condition: Bool, _ valueTrue: Int, _ valueFalse: Int) -> Int {
     return condition ? valueTrue : valueFalse
 }
+
+print(ifelse(true, 1, 0)) // Prints 1
+print(ifelse(false, 1, 0)) // Prints 0
+
+func ifelseV2(_ condition: Bool, _ valueTrue: Any, _ valueFalse: Any) -> Any {
+    return condition ? valueTrue : valueFalse
+}
+
+print(ifelseV2(true, 1, 0) as! Int) // Prints 1
+print(ifelseV2(false, 1, 0) as! Int) // Prints 0
