fc163d714b8d151c6eaf46bc1ad3dc3a6d8234ef
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-01-12 03:05:58 -0500

    Implements sorted two sum solution using two pointers approach

--- Algorithms.playground/Pages/Two Pointers Technique.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Two Pointers Technique.xcplaygroundpage/Contents.swift
@@ -18,3 +18,24 @@ func isPalindrome(_ string: String) -> Bool {
 }
 
 print(isPalindrome("aba"))
+
+// MARK: - Two sum with sorted array
+
+// Given a sorted array of unique integers and a target integer, return true if there exists a pair of numbers that sum to target, false otherwise.
+
+func twoSumSorted(_ array: [Int], target: Int) -> Bool {
+    var i = 0
+    var j = array.count - 1
+    while i < j {
+        if array[i] + array[j] > target {
+            j -= 1
+        } else if array[i] + array[j] < target {
+            i += 1
+        } else {
+            return true
+        }
+    }
+    return false
+}
+
+print(twoSumSorted([1, 2, 4, 6, 8, 9, 14, 15], target: 130)) // Prints true
