f73cf1dd14113c27fcab7178b1b9db16aec3e0ac
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-02-23 01:41:31 -0500

    Implements Balanced Array page

--- Algorithms.playground/Pages/Balanced Arrays.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Balanced Arrays.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
-//: [Previous](@previous)
+// Find an element in array such that sum of left array is equal to sum of right array
 
-import Foundation
+func balancedElement(from array: [Int]) -> Int? {
+    if array.count == 1 { return array.first }
 
-var greeting = "Hello, playground"
+    var startIndex = 0
+    var leftSum = 0
 
-//: [Next](@next)
+    var endIndex = array.count - 1
+    var rightSum = 0
+
+    for _ in 0 ..< array.count {
+        if startIndex == endIndex && leftSum == rightSum {
+            return array[startIndex]
+        }
+        if startIndex == endIndex && leftSum != rightSum {
+            return nil
+        }
+
+        if leftSum >= rightSum {
+            rightSum += array[endIndex]
+            endIndex -= 1
+        } else if rightSum > leftSum {
+            leftSum += array[startIndex]
+            startIndex += 1
+        }
+    }
+
+    return nil
+}
+
+print(balancedElement(from: [2, 3, 4, 1, 4, 5])) // Prints 1
