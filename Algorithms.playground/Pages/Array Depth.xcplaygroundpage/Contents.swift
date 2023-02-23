c44ae9a7af75ae5e1db570155303467b5bdd1195
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-02-23 02:17:26 -0500

    Adds additional implementation to Array Depth page

--- Algorithms.playground/Pages/Array Depth.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Array Depth.xcplaygroundpage/Contents.swift
@@ -10,3 +10,18 @@ extension Collection {
 
 print([[1, 2], [2, 3], [4, 5]].depth) // Prints 2
 print([[[1, 2]], [[2, 3]], [[4, 5]]].depth) // Prints 3
+
+// Given a multi-dimensional array, calculate the sum of the integer value multiplie by its depth
+
+func calculateDepthOperation(_ elements: Any, startIndex: Int = 0) -> Int {
+    guard let numbers = elements as? [Any] else {
+        return ((elements as? Int) ?? 0) * startIndex
+    }
+    var value = 0
+    for number in numbers {
+        value += calculateDepthOperation(number, startIndex: startIndex + 1)
+    }
+    return value
+}
+
+print(calculateDepthOperation([1, 1, [1, 1, [1]], 1])) // Prints 10 -> 1 + 1 + (1 * 2) + (1 * 2) + (1 * 3) + 1
