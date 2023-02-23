e1947faa93afa923a75a977473480a156f4d5298
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-02-23 01:12:27 -0500

    Adds flatMap implementation to Map Implementation page

--- Algorithms.playground/Pages/Map Implementation.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Map Implementation.xcplaygroundpage/Contents.swift
@@ -29,3 +29,18 @@ extension Collection {
 
 print([1, 2, 3].compactMap { $0 % 2 == 0 ? $0 * 2 : nil }) // Prints [4]
 print([1, 2, 3].compactMap2 { $0 % 2 == 0 ? $0 * 2 : nil }) // Prints [4]
+
+// MARK: - FlatMap
+
+extension Collection {
+    func flatMap2<T: Sequence>(_ transform: (Element) throws -> T) rethrows -> [T.Element] {
+        var temporalArray: [T.Element] = []
+        for item in self {
+            temporalArray.append(contentsOf: try transform(item))
+        }
+        return temporalArray
+    }
+}
+
+print([[1, 2, 3], [4, 5, 6], [7, 8, 9]].flatMap({ $0 })) // Prints [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print([[1, 2, 3], [4, 5, 6], [7, 8, 9]].flatMap2({ $0 })) // Prints [1, 2, 3, 4, 5, 6, 7, 8, 9]
