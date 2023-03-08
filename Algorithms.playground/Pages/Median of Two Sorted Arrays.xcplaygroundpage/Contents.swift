cfdb57622a5414ad7d6613255a45593f22077798
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-03-07 20:57:18 -0500

    Creates Median of Two Sorted Arrays page

---
+++ Algorithms.playground/Pages/Median of Two Sorted Arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+// Extracted from Leetcode: https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+// MARK: - Using sort
+
+func findMedianSortedArraysV1(_ nums1: [Int], _ nums2: [Int]) -> Double {
+    
+}
