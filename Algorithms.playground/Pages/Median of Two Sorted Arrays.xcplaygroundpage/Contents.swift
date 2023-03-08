4e6ac9986d015693455fd265a2c738dad3629e92
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-03-08 00:41:24 -0500

    Adds additional solution to Median of Two Sorted Arrays

--- Algorithms.playground/Pages/Median of Two Sorted Arrays.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Median of Two Sorted Arrays.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-// MARK: - Using sort
+// MARK: - Using sort method
 
 func findMedianSortedArraysV1(_ nums1: [Int], _ nums2: [Int]) -> Double {
     let mergedArray = (nums1 + nums2).sorted(by: { $0 < $1 })
@@ -20,3 +20,73 @@ func findMedianSortedArraysV1(_ nums1: [Int], _ nums2: [Int]) -> Double {
 
 print(findMedianSortedArraysV1([1, 3], [2])) // Prints 2
 print(findMedianSortedArraysV1([1, 2], [3, 4])) // Prints 2.5
+
+// MARK: - Without sort method using popFirst
+
+func findMedianSortedArraysV2(_ nums1: [Int], _ nums2: [Int]) -> Double {
+    var newArray: [Int] = []
+    var nums1 = nums1[nums1.indices]
+    var nums2 = nums2[nums2.indices]
+
+    while !nums1.isEmpty || !nums2.isEmpty {
+        guard let val1 = nums1.first else {
+            newArray.append(nums2.popFirst()!)
+            continue
+        }
+        guard let val2 = nums2.first else {
+            newArray.append(nums1.popFirst()!)
+            continue
+        }
+
+        if val1 > val2 {
+            newArray.append(val2)
+            nums2.popFirst()
+        } else {
+            newArray.append(val1)
+            nums1.popFirst()
+        }
+    }
+
+    let index = Int(Double(newArray.count / 2).rounded(.down))
+    if newArray.count % 2 == 0 {
+        return Double(newArray[index] + newArray[index - 1]) / 2.0
+    } else {
+        return Double(newArray[index])
+    }
+}
+
+//func findMedianSortedArraysV2(_ nums1: [Int], _ nums2: [Int]) -> Double {
+//    var newArray: [Int] = []
+//    var nums1 = nums1
+//    var nums2 = nums2
+//
+//    while !nums1.isEmpty || !nums2.isEmpty {
+//        guard let val1 = nums1.first else {
+//            nums1.popFirst()
+//            newArray.append(nums2.removeFirst())
+//            continue
+//        }
+//        guard let val2 = nums2.first else {
+//            newArray.append(nums1.removeFirst())
+//            newArray.drop
+//            continue
+//        }
+//
+//        if val1 > val2 {
+//            newArray.append(nums2.removeFirst())
+//        } else {
+//            newArray.append(nums1.removeFirst())
+//        }
+//    }
+//
+//    let index = Int(Double(newArray.count / 2).rounded(.down))
+//    if newArray.count % 2 == 0 {
+//        return Double(newArray[index] + newArray[index - 1]) / 2.0
+//    } else {
+//        return Double(newArray[index])
+//    }
+//}
+
+
+print(findMedianSortedArraysV2([1, 3], [2])) // Prints 2
+print(findMedianSortedArraysV2([1, 2], [3, 4])) // Prints 2.5
